4fb89fb60987cf67651899216066a64844a47c44
Jérôme CREMOUX <jcremoux@gmail.com>
2023-06-16 06:09:53 +0200

    Initial commit + test project

---
+++ FastDownloader/ParallelDownloader.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+
+public class ParallelDownloader
+{
+    public event Action DownloadStarted;
+    public event Action<string> DownloadCompleted;
+    public event Action<string> DownloadFailed;
+    public event Action<int> DownloadProgressChanged;
+
+    public async Task DownloadAsync(string url, string destinationPath, int maxParallelDownloads)
+    {
+        try
+        {
+            DownloadStarted?.Invoke();
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "HEAD";
+
+            long totalBytes = 0;
+
+            using (HttpWebResponse response = (HttpWebResponse)await request.GetResponseAsync())
+            {
+                totalBytes = response.ContentLength;
+            }
+
+            if (totalBytes <= 0)
+            {
+                throw new Exception("Impossible de récupérer la taille du fichier.");
+            }
+
+            DownloadProgressChanged?.Invoke(0);
+
+            int chunkSize = (int)Math.Ceiling((double)totalBytes / maxParallelDownloads);
+
+            string tempDirectory = Path.GetDirectoryName(destinationPath);
+
+            long totalDownloadedBytes = 0;
+
+            object lockObject = new object();
+
+            List<Task> downloadTasks = new List<Task>();
+
+            for (int chunkIndex = 0; chunkIndex < maxParallelDownloads; chunkIndex++)
+            {
+                int startRange = chunkIndex * chunkSize;
+                int endRange = startRange + chunkSize - 1;
+
+                string tempFilePath = Path.Combine(tempDirectory, $"{Path.GetRandomFileName()}.part");
+
+                downloadTasks.Add(Task.Run(async () =>
+                {
+                    try
+                    {
+                        long chunkDownloadedBytes = 0;
+
+                        HttpWebRequest rangeRequest = (HttpWebRequest)WebRequest.Create(url);
+                        rangeRequest.Method = "GET";
+                        rangeRequest.AddRange(startRange, endRange);
+
+                        using (HttpWebResponse rangeResponse = (HttpWebResponse)await rangeRequest.GetResponseAsync())
+                        using (Stream responseStream = rangeResponse.GetResponseStream())
+                        using (FileStream fileStream = new FileStream(tempFilePath, FileMode.Create, FileAccess.Write))
+                        {
+                            byte[] buffer = new byte[4096];
+                            int bytesRead;
+
+                            while ((bytesRead = await responseStream.ReadAsync(buffer, 0, buffer.Length)) > 0)
+                            {
+                                fileStream.Write(buffer, 0, bytesRead);
+                                chunkDownloadedBytes += bytesRead;
+
+                                lock (lockObject)
+                                {
+                                    totalDownloadedBytes += bytesRead;
+                                    int progress = (int)((totalDownloadedBytes * 100) / totalBytes);
+                                    OnDownloadProgressChanged(progress);
+                                }
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        DownloadFailed?.Invoke(ex.Message);
+                    }
+                }));
+            }
+
+            await Task.WhenAll(downloadTasks);
+
+            MergeTempFiles(tempDirectory, destinationPath);
+
+            DownloadCompleted?.Invoke(destinationPath);
+        }
+        catch (Exception ex)
+        {
+            DownloadFailed?.Invoke(ex.Message);
+        }
+    }
+
+    private void OnDownloadProgressChanged(int progress)
+    {
+        DownloadProgressChanged?.Invoke(progress);
+    }
+
+    private void MergeTempFiles(string tempDirectory, string destinationPath)
+    {
+        using (FileStream destinationStream = new FileStream(destinationPath, FileMode.Create, FileAccess.Write))
+        {
+            string[] tempFiles = Directory.GetFiles(tempDirectory, "*.part", SearchOption.TopDirectoryOnly);
+            Array.Sort(tempFiles);
+
+            foreach (string tempFilePath in tempFiles)
+            {
+                using (FileStream tempFileStream = new FileStream(tempFilePath, FileMode.Open, FileAccess.Read))
+                {
+                    tempFileStream.CopyTo(destinationStream);
+                }
+            }
+        }
+
+        // Supprimer les fichiers temporaires
+        foreach (string tempFilePath in Directory.GetFiles(tempDirectory, "*.part", SearchOption.TopDirectoryOnly))
+        {
+            File.Delete(tempFilePath);
+        }
+    }
+}
---
+++ FastDownloader/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace FastDownloader
+{
+    internal class Program
+    {
+        #region ATTRIBUTES
+        private static string _filePath = @"D:\";
+        private static string _fileName1GB = "debian-12.0.0-amd64-netinst.iso";
+        private static string _urlTest1GB = "https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/debian-12.0.0-amd64-netinst.iso";
+        #endregion
+
+        static void Main(string[] args)
+        {
+            Stopwatch sw = new Stopwatch();
+
+            string testDownloadFile = Path.Combine(_filePath, _fileName1GB);
+
+            ParallelDownloader downloader = new ParallelDownloader();
+            downloader.DownloadStarted += Handle_DownloadStarted;
+            downloader.DownloadFailed += Handle_DownloadFailed;
+            downloader.DownloadCompleted += Handle_DownloadCompleted;
+            downloader.DownloadProgressChanged += Handle_DownloadProgressChanged;
+
+            if (File.Exists(testDownloadFile))
+            {
+                File.Delete(testDownloadFile);  
+            }
+
+            Task.Run(async () =>
+            {
+                sw.Start();
+                await downloader.DownloadAsync(_urlTest1GB, testDownloadFile, 8);
+                sw.Stop();
+
+                Console.WriteLine(sw.Elapsed.ToString());
+            });
+
+            Console.ReadLine();
+
+        }
+
+        #region CALLBACKS
+        private static void Handle_DownloadProgressChanged(int progress)
+        {
+            Console.WriteLine(string.Format($"{progress} %"));
+        }
+
+        private static void Handle_DownloadCompleted(string obj)
+        {
+            Console.WriteLine(string.Format($"Download completed {obj}"));
+        }
+
+        private static void Handle_DownloadFailed(string obj)
+        {
+            Console.WriteLine(string.Format($"Download failed {obj}"));
+        }
+
+        private static void Handle_DownloadStarted()
+        {
+            Console.WriteLine(string.Format($"Download started..."));
+        }
+        #endregion
+    }
+}
